0385df7cdd53efccbac1827d6a3a616b83894ff1
tendril <alex.fulton.nz@gmail.com>
2023-08-25 12:19:31 +1200

    trivial cosmetic changes

--- module/light_display_po13/src/light_display_po13_internal.h
+++ module/light_display_po13/src/light_display_po13_internal.h
@@ -2,14 +2,15 @@
 #define _LIGHT_DISPLAY_PO13_INTERNAL_H
 
 // internal function signatures for portable platform I/O interface
+// TODO check pin assignment for i2c port, I think pin_cs might not be used
 extern void _platform_sh1107_i2c_port_init(
                                 uint8_t port_id, uint8_t pin_reset, uint8_t pin_cs,
                                 uint8_t pin_scl, uint8_t pin_sda);
 extern void _platform_sh1107_spi3_port_init(
                                 uint8_t port_id, uint8_t pin_reset, uint8_t pin_cs,
-                                uint8_t pin_sclk, uint8_t pin_mosi);
+                                uint8_t pin_sck, uint8_t pin_mosi);
 extern void _platform_sh1107_spi4_port_init(
                                 uint8_t port_id, uint8_t pin_reset, uint8_t pin_cs, 
-                                uint8_t pin_dc, uint8_t pin_sclk, uint8_t pin_mosi);
+                                uint8_t pin_dc, uint8_t pin_sck, uint8_t pin_mosi);
 
 #endif
--- module/light_display_po13/src/portable/host/platform.c
+++ module/light_display_po13/src/portable/host/platform.c
@@ -14,13 +14,13 @@ void _platform_sh1107_i2c_port_init(
 }
 void _platform_sh1107_spi3_port_init(
                                 uint8_t port_id, uint8_t pin_reset, uint8_t pin_cs,
-                                uint8_t pin_sclk, uint8_t pin_mosi)
+                                uint8_t pin_sck, uint8_t pin_mosi)
 {
         light_debug("spi port id 0x%x opened", port_id);
 }
 void _platform_sh1107_spi4_port_init(
                                 uint8_t port_id, uint8_t pin_reset, uint8_t pin_cs, 
-                                uint8_t pin_dc, uint8_t pin_sclk, uint8_t pin_mosi)
+                                uint8_t pin_dc, uint8_t pin_sck, uint8_t pin_mosi)
 {
         light_debug("spi port id 0x%x opened", port_id);
 }
